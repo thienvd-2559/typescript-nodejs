da516a049b0048f84332540647ff3ec62507267c
d10cn2btt <f2fe0310003428565e1e7bf150cd3baef23c496a>
2021-06-13 01:05:57 +0700

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+// initialize configuration
+dotenv.config();
 
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.APP_PORT; // default port to listen
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
