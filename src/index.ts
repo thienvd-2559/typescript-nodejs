20287a57856fe26dbb3fd58ad3356ea6abfc9e07
d10cn2btt <f2fe0310003428565e1e7bf150cd3baef23c496a>
2021-06-13 23:34:10 +0700

    add winston & makefile

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import logger from 'morgan';
+import path from 'path';
+
+import indexRouter from './routes/index';
+import winston from './config/winston';
 
 // initialize configuration
 dotenv.config();
 
 const app = express();
-const port = process.env.APP_PORT; // default port to listen
 
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/abc', indexRouter);
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Running nodejs with docker');
+  res.send('Running nodejs with docker!');
 });
 
+// // catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next('dddd');
+  // next(createError(404));
+});
+
+// error handler
+app.use((param, req, res, next) => {
+  winston.error(param);
+
+  // set locals, only providing error in development
+  res.locals.message = 'abc';
+  res.locals.error = req.app.get('env') === 'development' ? 'err' : {};
+
+  // render the error page
+  res.status(404);
+  res.send({ message: 'sai cmnr' });
+});
+
+const port = process.env.APP_PORT; // default port to listen
 // start the Express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router} from 'express';
+
+const router = Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.json({ title: 'Express' });
+});
+
+export default router;
